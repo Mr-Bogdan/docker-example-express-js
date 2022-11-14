3cc73a36bf4674563c343fe84a99dd3f55bef30d
Bohdan <62215279+Mr-Bogdan@users.noreply.github.com>
2022-11-14 08:07:49 +0000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World MAN!')
+    res.send('Hello World MANs!')
 })
 
 app.get('/students', (req, res) => {
